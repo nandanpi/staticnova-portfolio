23ba85b116241bb9fc32d0c7a79314ceab5e7b9b
Nandan R Pai <51937330+nandanpi@users.noreply.github.com>
2023-06-11 16:03:46 +0530

    Added About Section

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React,{useState} from "react";
 import {HashLink} from "react-router-hash-link";
 const Navbar = () => {
+    const [nav,setNav] = useState(false);
+    function toggleNav(){
+        setNav(!nav);
+    }
     return(
         <>
-            <div className="flex justify-between text-white">
+            <div className="hidden md:flex justify-between text-white">
                 <div className="px-6 py-4">
                     <span className="arialb text-3xl">Staticnova</span>
                 </div>
