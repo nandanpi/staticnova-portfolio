e5270be3de3bd2888b89de0ea1457ab2afc5ebdd
Nandan R Pai <51937330+nandanpi@users.noreply.github.com>
2023-06-11 14:16:51 +0530

    Init Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Main from "./components/Main";
+import Navbar from "./components/navbar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="bg-gradient-to-b from-[#030B31] to-[#33439C]">
+        <Navbar />
+        <Main />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {HashLink} from "react-router-hash-link";
+const Navbar = () => {
+    return(
+        <>
+            <div className="flex justify-between text-white">
+                <div className="px-6 py-4">
+                    <span className="arialb text-4xl">Staticnova</span>
+                </div>
+                <div className="px-6 py-4">
+                    <ul className="flex space-x-6 text-4xl ocra">
+                        <li><HashLink smooth to="/#works">Works</HashLink></li>
+                        <li>About Me</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
+
+}
+export default Navbar;
